4fd3a28261bf863375d7b81be133391368edb9e7
iscra-spark <58892507+iscra-spark@users.noreply.github.com>
2021-04-22 17:42:52 +0300

    deleted charts

--- a-b-testing/src/App.js
+++ a-b-testing/src/App.js
@@ -1,7 +1,6 @@
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import { ThemeProvider } from '@material-ui/core';
 import GlobalStyles from 'src/components/GlobalStyles';
-import 'src/mixins/chartjs';
 import theme from 'src/theme';
 import Login from 'src/pages/Login';
 import MainLayout from 'src/components/MainLayout';
