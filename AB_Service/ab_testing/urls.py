ed3cdfad71743d57e338d220952e0b3f08ace0d9
Vlad Medvid <57641251+Drakonchik2@users.noreply.github.com>
2021-05-30 17:34:40 +0300

    Delete urls.py

--- AB_Service/ab_testing/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from ab_testing import views
-
-urlpatterns = [
-    url(r'^api/ab_testing$', views.offers_list),
-    url(r'^api/ab_testing/(?P<pk>[0-9]+)$', views.offer_detail),
-]
