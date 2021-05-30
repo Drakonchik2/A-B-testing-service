6b424f3de8c2992e892593e48e5569da1710a80a
Vlad Medvid <57641251+Drakonchik2@users.noreply.github.com>
2021-05-30 17:35:47 +0300

    Create admin.py

---
+++ ab_service/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Offer, Prealand
+
+
+class PrealandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'link', 'date_time')
+    list_filter = ('offer', 'date_time')
+    fields = ['offer', 'name', 'link', 'date_time']
+    search_fields = ['name']
+
+
+class PrealandInline(admin.TabularInline):
+    model = Prealand
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_time')
+    list_filter = ('name', 'date_time')
+    fields = ['name', 'date_time', 'closing_link']
+    search_fields = ['name']
+    inlines = [PrealandInline]
+
+
+admin.site.register(Offer, OfferAdmin)
+admin.site.register(Prealand, PrealandAdmin)
