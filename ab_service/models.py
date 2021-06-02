1a98a40ec1d610b7cfc6112ff5e7d01329eb7773
Vlad Medvid <57641251+Drakonchik2@users.noreply.github.com>
2021-06-02 17:28:25 +0300

    Update models.py

--- ab_service/models.py
+++ ab_service/models.py
@@ -10,7 +10,7 @@ class Offer(models.Model):
     date_time = models.DateTimeField(default=timezone.now)
     closing_link = models.CharField(max_length=75)
 
-
+   
 class Prealand(models.Model):
     offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
     name = models.CharField(max_length=75)
