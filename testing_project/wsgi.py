9a70b1c6e92c00775c3e54ae86e42c2a258cd79f
Vlad Medvid <57641251+Drakonchik2@users.noreply.github.com>
2021-07-07 12:23:05 +0300

    Update wsgi.py

--- testing_project/wsgi.py
+++ testing_project/wsgi.py
@@ -1,9 +1,16 @@
-import os
-import sys
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+"""
+WSGI config for testing_project project.
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testing_project.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
